9c723fa4bb3e53ddfa55de5050c917f5a9894a33
Paul Moss <moss.paul@gmail.com>
2012-03-28 19:50:43 -0400

    initial import

---
+++ settings.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Paul Moss"
+SITENAME = u":w! Ramblings"
+SITEURL = '/'
+
+GITHUB_URL = 'https://github.com/ScorpionResponse/'
+PDF_GENERATOR = False
+
+TIMEZONE = 'America/New York'
+DEFAULT_LANG='en'
+
+BYLINE = '&copy; 2012 Paul Moss &amp; co. All Rights Reserved.'
+# Blogroll
+LINKS =  (
+    ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+    ('Python.org', 'http://python.org'),
+    ('Jinja2', 'http://jinja.pocoo.org'),
+         )
+
+# Social widget
+SOCIAL = (
+          ('You can add links in your config file', '#'),
+         )
+
+DEFAULT_PAGINATION = 7
+
+
+    
