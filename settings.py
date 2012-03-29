9a92756d5e9f8e54341c8038f9c34affb6de54cc
Paul Moss <moss.paul@gmail.com>
2012-03-28 21:00:31 -0400

    REmove blog junk

--- settings.py
+++ settings.py
@@ -12,17 +12,6 @@ TIMEZONE = 'America/New_York'
 DEFAULT_LANG='en'
 
 BYLINE = '&copy; 2012 Paul Moss &amp; co. All Rights Reserved.'
-# Blogroll
-LINKS =  (
-    ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-    ('Python.org', 'http://python.org'),
-    ('Jinja2', 'http://jinja.pocoo.org'),
-         )
-
-# Social widget
-SOCIAL = (
-          ('You can add links in your config file', '#'),
-         )
 
 DEFAULT_PAGINATION = 7
 
