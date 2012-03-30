84b852c61f896610949620d1434c19a078ffdd19
Paul Moss <moss.paul@gmail.com>
2012-03-29 21:32:11 -0400

    A bunch of changes that I won't bother to document

---
+++ content/pages/resume.rst
@@ -0,0 +1,4 @@
+My Resume
+#########
+
+I work in an office as a Product Manager.  I know computer stuff.
--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@
 
 AUTHOR = u"Paul Moss"
 SITENAME = u":w! Ramblings"
+#SITESUBTITLE = u"Writings of a guy on the internet"
 SITEURL = '/'
 
 GITHUB_URL = 'https://github.com/ScorpionResponse/'
@@ -11,9 +12,8 @@ PDF_GENERATOR = False
 TIMEZONE = 'America/New_York'
 DEFAULT_LANG='en'
 
-BYLINE = '&copy; 2012 Paul Moss &amp; co. All Rights Reserved.'
+BYLINE = '&copy; 2012 Paul Moss. All Rights Reserved.'
 
 DEFAULT_PAGINATION = 7
 
-
-    
+GOOGLE_ANALYTICS = "UA-30458541-1"
