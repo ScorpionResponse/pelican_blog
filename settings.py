179df7450513d84898a520684dffd87f9ad2c797
Paul Moss <moss.paul@gmail.com>
2016-03-05 14:09:30 +0000

    Upgrade pelican version

--- settings.py
+++ settings.py
@@ -3,14 +3,14 @@
 
 AUTHOR = u"Paul Moss"
 SITENAME = u":w! Ramblings"
-#SITESUBTITLE = u"Writings of a guy on the internet"
+# SITESUBTITLE = u"Writings of a guy on the internet"
 SITEURL = 'http://scorpionresponse.com'
 
 GITHUB_URL = 'https://github.com/ScorpionResponse/'
 PDF_GENERATOR = False
 
 TIMEZONE = 'America/New_York'
-DEFAULT_LANG='en'
+DEFAULT_LANG = 'en'
 
 BYLINE = '&copy; 2012 Paul Moss. All Rights Reserved.'
 
@@ -18,11 +18,18 @@ DEFAULT_PAGINATION = 7
 
 GOOGLE_ANALYTICS = "UA-30458541-1"
 
-#CLEAN_URLS = True
+# CLEAN_URLS = True
 
-FILES_TO_COPY = (('extra/robots.txt', 'robots.txt'),
-                 ('extra/favicon.ico', 'favicon.ico'),
-                 ('extra/CNAME', 'CNAME'),)
+# FILES_TO_COPY = (('extra/robots.txt', 'robots.txt'),
+#                 ('extra/favicon.ico', 'favicon.ico'),
+#                 ('extra/CNAME', 'CNAME'),)
+
+STATIC_PATHS = ['extra']
+
+EXTRA_PATH_METADATA = {'extra/robots.txt': {'path': 'robots.txt'},
+                       'extra/favicon.ico': {'path': 'favicon.ico'},
+                       'extra/CNAME': {'path': 'CNAME'}
+                       }
 
 ARTICLE_URL = 'posts/{slug}.html'
 ARTICLE_SAVE_AS = 'posts/{slug}.html'
